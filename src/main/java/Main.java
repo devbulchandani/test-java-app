51f108f34925b77c3c88daeff3205eed88aa0fd1
Dev Bulchandani <devbulchandani8@gmail.com>
2026-02-09 16:20:02 +0530

    Ledger

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,14 @@
 public class Main {
 
     public static void main(String[] args) {
-        Transaction t1 = new Transaction("Salary", 50000, "Income");
-        Transaction t2 = new Transaction("Groceries", 2000, "Expense");
+        Ledger ledger = new Ledger();
 
-        t1.printDetails();
-        t2.printDetails();
+        ledger.addTransaction(new Transaction("Salary", 50000, "Income"));
+        ledger.addTransaction(new Transaction("Groceries", 2000, "Expense"));
+        ledger.addTransaction(new Transaction("Freelance", 12000, "Income"));
+        ledger.addTransaction(new Transaction("Rent", 15000, "Expense"));
+
+        ledger.printLedger();
     }
 
 }
